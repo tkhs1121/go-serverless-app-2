248b6955c24c445aa6e3c290b1bdaf6ef843281c
tkhs1121 <tkhs1121@protonmail.com>
2022-06-15 19:02:43 +0900

    dbモデルを作成

--- model.go
+++ model.go
@@ -1,9 +1,20 @@
 package main
 
+import "github.com/aws/aws-sdk-go/service/dynamodb"
+
 type Payload struct {
 	Body string `json:"body"`
 }
 
 type Request struct {
 	URL string `json:"url"`
+}
+
+type DBAPI interface {
+	putItem(url string) error
+}
+
+type DB struct {
+	DDB   *dynamodb.DynamoDB
+	DBAPI DBAPI
 }
